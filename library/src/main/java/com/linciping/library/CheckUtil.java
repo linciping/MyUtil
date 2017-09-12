25ef0d62499e909ab7ab7883c0e41e1666d61884
linciping <linciping@outlook.com>
2017-09-12 11:01:21 +0800

    分离检查工具和通用工具

--- library/src/main/java/com/linciping/library/CheckUtil.java
+++ library/src/main/java/com/linciping/library/CheckUtil.java
@@ -2,13 +2,13 @@ package com.linciping.library;
 
 import java.util.Collection;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * @author linciping
- *         2017/9/7
- *         检查工具
+ * 2017/9/7
+ * 检查工具
  */
-
 public class CheckUtil {
     /**
      * 判断字符串是否为空
@@ -41,30 +41,6 @@ public class CheckUtil {
     }
 
     /**
-     * 处理字符串
-     *
-     * @param content 需要处理的字符串
-     * @return 如果为空，返回""，不然返回content
-     */
-    public static String commonString(String content) {
-        return commonString(content, "");
-    }
-
-    /**
-     * 处理字符串，
-     *
-     * @param content        需要处理的字符串
-     * @param defaultContent 默认字符
-     * @return 如果为空，返回defaultContent，不然返回content
-     */
-    public static String commonString(String content, String defaultContent) {
-        if (isStringEmpty(content)) {
-            return defaultContent;
-        }
-        return content;
-    }
-
-    /**
      * 判断对象数组是否为空
      *
      * @param array 需要判断的对象数组
@@ -73,5 +49,15 @@ public class CheckUtil {
      */
     public static <T> boolean isArrayEmpty(T[] array) {
         return array == null || array.length <= 0;
+    }
+
+    /**
+     * 判断set是否为空
+     *
+     * @param set 需要判断的set对象
+     * @return 如果set为空返回 true,不然返回false
+     */
+    public static boolean isSetEmpty(Set<?> set) {
+        return set == null || set.size() <= 0;
     }
 }
---
+++ library/src/main/java/com/linciping/library/CommonUtil.java
@@ -0,0 +1,91 @@
+package com.linciping.library;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * @author linciping
+ * @time 2017/9/12
+ * @note
+ */
+
+public class CommonUtil {
+
+    /**
+     * 处理字符串 See {@link CheckUtil#isStringEmpty(String)}
+     *
+     * @param content 需要处理的字符串
+     * @return 如果为空，返回""，不然返回content
+     */
+    public static String commonString(String content) {
+        return commonString(content, "");
+    }
+
+    /**
+     * 处理字符串
+     *
+     * @param content        需要处理的字符串
+     * @param defaultContent 默认字符
+     * @return 如果为空，返回defaultContent，不然返回content
+     */
+    public static String commonString(String content, String defaultContent) {
+        if (CheckUtil.isStringEmpty(content)) {
+            return defaultContent;
+        }
+        return content;
+    }
+
+    /**
+     * 返回集合的大小 Use {@link CheckUtil#isCollectionEmpty(Collection)}
+     *
+     * @param collection 目标集合
+     * @return 集合的大小，如果为空为0 ，不然返回集合的大小
+     */
+    public static int collectionSize(Collection<?> collection) {
+        if (CheckUtil.isCollectionEmpty(collection)) {
+            return 0;
+        }
+        return collection.size();
+    }
+
+    /**
+     * 返回数组的大小  Use {@link CheckUtil#isArrayEmpty(T[])}
+     *
+     * @param array 目标数组
+     * @param <T>   泛型
+     * @return 数组的大小，如果为空为0，不然返回数组的大小
+     */
+    public static <T> int arraySize(T[] array) {
+        if (CheckUtil.isArrayEmpty(array)) {
+            return 0;
+        }
+        return array.length;
+    }
+
+    /**
+     * 返回map的大小 Use {@link CheckUtil#isMapEmpty(Map)}
+     *
+     * @param map 目标map
+     * @return map的大小，如果为空为0 ，不然返回map的大小
+     */
+    public static int mapSize(Map<?, ?> map) {
+        if (CheckUtil.isMapEmpty(map)) {
+            return 0;
+        }
+        return map.size();
+    }
+
+    /**
+     * 返回set的大小 Use {@link CheckUtil#isSetEmpty(Set)}
+     *
+     * @param set 目标set
+     * @return set的大小，如果为空为0 ，不然返回set的大小
+     */
+    public static int setSize(Set<?> set) {
+        if (CheckUtil.isSetEmpty(set)) {
+            return 0;
+        }
+        return set.size();
+    }
+}
